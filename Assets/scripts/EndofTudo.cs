93b80a53f1a5f4708735ec9887c6247333eb385b
Gabrielcereja64 <beckergabriel341@gmail.com>
2021-03-11 22:06:15 -0300

    projeto criado

---
+++ Assets/scripts/EndofTudo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndofTudo : MonoBehaviour
+{
+    
+    public void endOfgame()
+    {
+        //Debug.Log("HIT");
+        //Application.Quit();
+        SceneManager.LoadScene("final");
+    }
+    public void reallyEndOfGame()
+    
+    {
+        
+     Application.Quit();
+
+    
+    }
+
+}
